34ebadd30925dcab85b0571d7086c06cc0ab70b4
Mark Bastian <markbastian@gmail.com>
2017-09-20 06:42:07 -0600

    deleted temp files.

--- resources/public/js/compiled/out/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [figwheel.client] [figwheel.client.utils] [flocking.core]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/flocking.core.on-js-reload (apply js/flocking.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'flocking.core/on-js-reload' is missing"))), :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
